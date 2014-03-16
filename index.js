309ba6d9a1889ff7e198e3cea9278e43973dbd79
Daniel Lamando <dan@danopia.net>
2014-03-16 13:03:24 -0700

    ddwrt status parsing, for wifi station strengths
    Nothing pretty, don’t worry.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var spawn = require('child_process').spawn;
+/*var spawn = require('child_process').spawn;
 var proc = spawn('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', ['en1', 'scan']);
 
 var output = '';
@@ -14,4 +14,32 @@ proc.on('close', function () {
   });
 
   console.log(nets);
+});*/
+
+require('http').get('http://root:tits@192.168.1.1/Info.live.htm', function (res) {
+  if (res.statusCode != 200) {
+    console.log('Router returned code', res.statusCode);
+    return;
+  }
+
+  var raw = '';
+  res.on('data', function (chunk) {
+    raw += chunk;
+  }).setEncoding('utf8');
+  res.on('end', function () {
+    var props = {};
+    raw.replace(/\n$/, '').split('\n').forEach(function (line) {
+      var match = line.match(/\{([a-z_]+)::(.*)\}/);
+
+      if (match[2][0] == "'") {
+        props[match[1]] = match[2].substr(1, match[2].length - 2).split("','");
+      } else {
+        props[match[1]] = match[2];
+      }
+    });
+
+    console.log(props);
+  });
+}).on('error', function (e) {
+
 });
