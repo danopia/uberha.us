b16191707e42abe4e4210da58be4586450a151af
Daniel Lamando <dan@danopia.net>
2014-03-30 11:42:08 -0700

    Actually implement bare occupancy
    Only pays attention to which MACs are online.

--- occupancy/index.js
+++ occupancy/index.js
@@ -2,15 +2,47 @@ var input = require('./input');
 
 var node = new (require('./../node'))();
 node.start('home.sensors', 'occupancy', function () {
-  node.addProperty('people_home',  'number');
-  node.addProperty('people_awake', 'number');
+  node.addProperty('people_home',  'number', 0);
+  node.addProperty('people_awake', 'number', 0);
 
-  setInterval(function () {
-    node.setProperty('people_home',  2);
-    node.setProperty('people_awake', 1);
-  }, 2500);
-});
+  var people = [
+    {name: 'Daniel', laptops: ['28:CF:E9:5A:26:41'], phones: ['F8:A9:D0:0E:E1:31']},
+    {name: 'Raynes', laptops: [                   ], phones: ['04:F7:E4:14:C6:A4']}
+  ];
+  var lHome = 0, lAwake = 0;
 
-input.getStations(function (stations) {
-  //console.log(stations);
+  var refresh = function () {
+    input.getStations(function (stations) {
+      var home = 0, awake = 0;
+
+      var macs = stations.map(function (station) {
+        return station[0];
+      });
+
+      people.forEach(function (person) {
+        var state = 0;
+
+        person.phones.forEach(function (mac) {
+          if (macs.indexOf(mac) != -1) state = 1;
+        });
+        person.laptops.forEach(function (mac) {
+          if (macs.indexOf(mac) != -1) state = 2;
+        });
+
+        if (state > 0) home++;
+        if (state > 1) awake++;
+      });
+
+      if (home != lHome)
+        node.setProperty('people_home',  home);
+      if (awake != lAwake)
+        node.setProperty('people_awake', awake);
+
+      lHome = home;
+      lAwake = awake;
+    });
+  };
+
+  setInterval(refresh, 2500);
+  refresh();
 });
